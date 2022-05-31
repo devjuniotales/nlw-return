91eac05f593d0027a88482fe4937de3af84ea8aa
devjuniotales <junio.thales@hotmail.com>
2022-05-30 22:02:19 -0300

    all

---
+++ mobile/App.tsx
@@ -0,0 +1,35 @@
+import 'react-native-gesture-handler'
+import React from 'react';
+import { StatusBar } from "expo-status-bar";
+import { View } from "react-native";
+import { useFonts, Inter_400Regular , Inter_500Medium } from '@expo-google-fonts/inter';
+
+import  Widget  from "./src/components/Widget";
+import { theme } from "./src/theme";
+
+
+export default function App() {
+  const [ fontsLoaded ] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium
+  });
+
+  if (!fontsLoaded) {
+  }
+
+  return (
+    <View style ={{
+      flex : 1,
+      backgroundColor : theme.colors.background
+    }}>
+      <Widget/>
+
+      <StatusBar 
+      style="light"
+      backgroundColor="transparent"
+      translucent
+      />
+    </View>
+  );
+}
+
